264c796f88f64d00c3e055d90808eb6ca3f03f77
Douglas Felipe Bussmann Siedschlag <douglasfelipebs@hotmail.com>
2018-10-12 15:25:25 -0300

    Adicionado funcoionalidade 'Fatorial' solicitada na quesão c)

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
     static Scanner s = new Scanner(System.in);
     static String menu = "Informe uma operação a ser realizada:"
             + "\n 1 - Tabuada;"
+            + "\n 2 - Fatorial;"
             + "\n 0 - Sair;";
     
     public static void main(String[] args) {
@@ -28,6 +29,23 @@ public class Main {
             switch(opcao){
                 case 1:{
                     tabuada();
+                    break;
+                }
+                case 2:{
+                    System.out.println("Informe o número a ser mutiplicado:");
+                    String numeroToConvert = s.nextLine();
+                    int numero = 0;
+                    try {
+                        numero = Integer.parseInt(numeroToConvert);
+                    } catch (Exception e) {
+                        System.out.println("Número inválido! (O valor deve ser numérico)");
+                        break;
+                    }
+                    if(numero < 0){
+                        System.out.println("Número inválido! (O valor deve ser positivo)");
+                        break;
+                    }
+                    System.out.println(fatorial(1, numero));
                     break;
                 }
                 case 0:{
@@ -50,11 +68,20 @@ public class Main {
         } catch (Exception e) {
         }
         if(numero == Integer.MIN_VALUE){
-            System.out.println("Informe um valor numérico válido!");
+            System.out.println("Número inválido! (O valor deve ser numérico)");
         } else {
             for (int i = 1; i <= 10; i++) {
                 System.out.println(i + "\tx\t" + numero + "\t=\t" + (i*numero));
             }
         }
+    }
+    
+    public static int fatorial(int total, int valor){
+        if(valor == 1){
+            return total;
+        }
+        total *= valor;
+        valor--;
+        return fatorial(total, valor);
     }
 }
